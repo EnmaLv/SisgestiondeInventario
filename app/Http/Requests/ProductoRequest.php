1a91a817013b7141a4f8b461714e2ffac40b40bb
Angel <angeljlinarez@gmail.com>
2025-08-18 17:26:44 -0400

    Creacion de archivos request para una mejor validacion en categoria y producto

---
+++ app/Http/Requests/ProductoRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'categoria_id' => 'required|exists:categorias,id',
+            'codigo' => 'required|string|max:255',
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'required|string',
+            'imagen' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'precio_compra' => 'required|numeric',
+            'precio_venta' => 'required|numeric',
+            'stock_minimo' => 'required|integer',
+            'stock_maximo' => 'required|integer',
+            'unidad_medida' => 'required',
+            'estado' => 'required|boolean',
+        ];
+    }
+}
