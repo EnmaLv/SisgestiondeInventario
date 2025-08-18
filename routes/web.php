c89a784da16399e96c4096f7112be2ac72b53724
Angel <angeljlinarez@gmail.com>
2025-08-18 17:04:46 -0400

    Mejora de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\SucursalController;
+use App\Http\Controllers\ProductoController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,31 +18,58 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 //Rutas para Maestros
 
 //Categorias
-Route::get('/admin/maestros/categorias', [App\Http\Controllers\CategoriaController::class, 'index'])->name('admin.maestros.categorias.index')->middleware('auth');
-Route::get('/admin/maestros/categorias/create', [App\Http\Controllers\CategoriaController::class, 'create'])->name('admin.maestros.categorias.create')->middleware('auth');
-Route::post('/admin/maestros/categorias/store', [App\Http\Controllers\CategoriaController::class, 'store'])->name('admin.maestros.categorias.store')->middleware('auth');
-Route::get('/admin/maestros/categorias/{categoria}', [App\Http\Controllers\CategoriaController::class, 'show'])->name('admin.maestros.categorias.show')->middleware('auth');
-Route::get('/admin/maestros/categorias/{categoria}/edit', [App\Http\Controllers\CategoriaController::class, 'edit'])->name('admin.maestros.categorias.edit')->middleware('auth');
-Route::put('/admin/maestros/categorias/{categoria}', [App\Http\Controllers\CategoriaController::class, 'update'])->name('admin.maestros.categorias.update')->middleware('auth');
-Route::delete('/admin/maestros/categorias/{categoria}', [App\Http\Controllers\CategoriaController::class, 'destroy'])->name('admin.maestros.categorias.destroy');
+
+Route::prefix('/admin/maestros/categorias')->group(function () {
+    //Index 
+    Route::get('/', [CategoriaController::class, 'index'])->name('admin.maestros.categorias.index');
+    //Crear una nueva categoría
+    Route::get('/create', [CategoriaController::class, 'create'])->name('admin.maestros.categorias.create');
+    //Almacenar una nueva categoría
+    Route::post('/store', [CategoriaController::class, 'store'])->name('admin.maestros.categorias.store');
+    //Mostrar una categoría específica
+    Route::get('/{categoria}', [CategoriaController::class, 'show'])->name('admin.maestros.categorias.show');
+    //Editar una categoría específica
+    Route::get('/{categoria}/edit', [CategoriaController::class, 'edit'])->name('admin.maestros.categorias.edit');
+    //Actualizar una categoría específica
+    Route::put('/{categoria}', [CategoriaController::class, 'update'])->name('admin.maestros.categorias.update');
+    //Eliminar una categoria especifica
+    Route::delete('/{categoria}', [CategoriaController::class, 'destroy'])->name('admin.maestros.categorias.destroy');
+
+})->middleware('auth');
+
+
 
 //Sucursales
-Route::get('/admin/maestros/sucursales', [App\Http\Controllers\SucursalController::class, 'index'])->name('admin.maestros.sucursales.index')->middleware('auth');
-Route::get('/admin/maestros/sucursales/create', [App\Http\Controllers\SucursalController::class, 'create'])->name('admin.maestros.sucursales.create')->middleware('auth');
-Route::post('/admin/maestros/sucursales/store', [App\Http\Controllers\SucursalController::class, 'store'])->name('admin.maestros.sucursales.store')->middleware('auth');
-Route::get('/admin/maestros/sucursales/{sucursal}', [App\Http\Controllers\SucursalController::class, 'show'])->name('admin.maestros.sucursales.show')->middleware('auth');
-Route::get('/admin/maestros/sucursales/{sucursal}/edit', [App\Http\Controllers\SucursalController::class, 'edit'])->name('admin.maestros.sucursales.edit')->middleware('auth');
-Route::put('/admin/maestros/sucursales/{sucursal}', [App\Http\Controllers\SucursalController::class, 'update'])->name('admin.maestros.sucursales.update')->middleware('auth');
-Route::delete('/admin/maestros/sucursales/{sucursal}', [App\Http\Controllers\SucursalController::class, 'destroy'])->name('admin.maestros.sucursales.destroy');
+Route::prefix('/admin/maestros/sucursales')->middleware('auth')->group(function () {
+    Route::get('/', [SucursalController::class, 'index'])->name('admin.maestros.sucursales.index');
+    Route::get('/create', [SucursalController::class, 'create'])->name('admin.maestros.sucursales.create');
+    Route::post('/store', [SucursalController::class, 'store'])->name('admin.maestros.sucursales.store');
+    Route::get('/{sucursal}', [SucursalController::class, 'show'])->name('admin.maestros.sucursales.show');
+    Route::get('/{sucursal}/edit', [SucursalController::class, 'edit'])->name('admin.maestros.sucursales.edit');
+    Route::put('/{sucursal}', [SucursalController::class, 'update'])->name('admin.maestros.sucursales.update');
+    Route::delete('/{sucursal}', [SucursalController::class, 'destroy'])->name('admin.maestros.sucursales.destroy');
+});
 
 //Productos
-Route::get('/admin/maestros/productos', [App\Http\Controllers\ProductoController::class, 'index'])->name('admin.maestros.productos.index')->middleware('auth');
-Route::get('/admin/maestros/productos/create', [App\Http\Controllers\ProductoController::class, 'create'])->name('admin.maestros.productos.create')->middleware('auth');
-Route::post('/admin/maestros/productos/store', [App\Http\Controllers\ProductoController::class, 'store'])->name('admin.maestros.productos.store')->middleware('auth');
-Route::get('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'show'])->name('admin.maestros.productos.show')->middleware('auth');
-Route::get('/admin/maestros/productos/{producto}/edit', [App\Http\Controllers\ProductoController::class, 'edit'])->name('admin.maestros.productos.edit')->middleware('auth');
-Route::put('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'update'])->name('admin.maestros.productos.update')->middleware('auth');
-Route::delete('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'destroy'])->name('admin.maestros.productos.destroy');
+
+Route::prefix('/admin/maestros/productos')->middleware('auth')->group(function () {
+    Route::get('/', [ProductoController::class, 'index'])->name('admin.maestros.productos.index');
+    Route::get('/create', [ProductoController::class, 'create'])->name('admin.maestros.productos.create');
+    Route::post('/store', [ProductoController::class, 'store'])->name('admin.maestros.productos.store');
+    Route::get('/{producto}', [ProductoController::class, 'show'])->name('admin.maestros.productos.show');
+    Route::get('/{producto}/edit', [ProductoController::class, 'edit'])->name('admin.maestros.productos.edit');
+    Route::put('/{producto}', [ProductoController::class, 'update'])->name('admin.maestros.productos.update');
+    Route::delete('/{producto}', [ProductoController::class, 'destroy'])->name('admin.maestros.productos.destroy');
+});
+
+// Route::get('/admin/maestros/productos', [App\Http\Controllers\ProductoController::class, 'index'])->name('admin.maestros.productos.index')->middleware('auth');
+
+// Route::get('/admin/maestros/productos/create', [App\Http\Controllers\ProductoController::class, 'create'])->name('admin.maestros.productos.create')->middleware('auth');
+// Route::post('/admin/maestros/productos/store', [App\Http\Controllers\ProductoController::class, 'store'])->name('admin.maestros.productos.store')->middleware('auth');
+// Route::get('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'show'])->name('admin.maestros.productos.show')->middleware('auth');
+// Route::get('/admin/maestros/productos/{producto}/edit', [App\Http\Controllers\ProductoController::class, 'edit'])->name('admin.maestros.productos.edit')->middleware('auth');
+// Route::put('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'update'])->name('admin.maestros.productos.update')->middleware('auth');
+// Route::delete('/admin/maestros/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'destroy'])->name('admin.maestros.productos.destroy');
 
 //Rutas para Movimientos
 Route::get('/admin/movimientos/transacciones', [App\Http\Controllers\TransaccionController::class, 'index'])->name('admin.movimientos.transacciones.index')->middleware('auth');
