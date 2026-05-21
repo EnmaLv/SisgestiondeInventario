f7716b16f95ab67b9288e5c64234c24d90af5431
EnmaLv <medina.enma1234@gmail.com>
2026-05-20 21:44:33 -0400

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(\App\Http\Middleware\IgnorarTasaHoy::class);
-
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'tasa.actualizada' => CheckTasaActualizada::class,
         ]);
