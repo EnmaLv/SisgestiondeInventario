19acbba8933b53da0acba3e41d326c3c85119e9c
Nohely <nohelysq2006@gmail.com>
2026-07-05 15:26:44 -0400

    Cambios de Enma Departamento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            DepartamentoSeeder::class,
             EstatusSeeder::class,
             UnidadSeeder::class,
             ModulosSeeder::class,
