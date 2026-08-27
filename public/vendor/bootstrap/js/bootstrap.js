7fcb34d7463b91ea2e508119a4c51ed5f1f5a02b
EnmaLv <medina.enma1234@gmail.com>
2026-08-26 20:49:48 -0400

    Migrando a Pusher para el chat

--- public/vendor/bootstrap/js/bootstrap.js
+++ public/vendor/bootstrap/js/bootstrap.js
@@ -10,18 +10,15 @@
 })(this, (function (exports, $, Popper) { 'use strict';
 
 
-  if (import.meta.env.VITE_REVERB_APP_KEY && import.meta.env.VITE_REVERB_APP_KEY !== 'disabled') {
+  if (import.meta.env.VITE_PUSHER_APP_KEY) {
       window.Echo = new Echo({
-          broadcaster: 'reverb',
-          key: import.meta.env.VITE_REVERB_APP_KEY,
-          wsHost: import.meta.env.VITE_REVERB_HOST,
-          wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-          wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-          forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
-          enabledTransports: ['ws', 'wss'],
+          broadcaster: 'pusher',
+          key: import.meta.env.VITE_PUSHER_APP_KEY,
+          cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+          forceTLS: true,
       });
   }
-  
+
   function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
   var $__default = /*#__PURE__*/_interopDefaultLegacy($);
