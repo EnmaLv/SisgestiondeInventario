b721d8290dddd359e1860a002ee41a1e897ff836
EnmaLv <medina.enma1234@gmail.com>
2026-08-26 20:12:10 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/css/general.css',
+                'resources/css/gerenal.css',
                 'resources/js/app.js',
             ],
             refresh: true,
